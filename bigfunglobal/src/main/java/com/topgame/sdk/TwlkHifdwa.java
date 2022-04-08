2c7f7d6bebd7788790d3504289a0b9434e8c9ad2
yanwenqiang <dream@hzyouliang.com>
2022-04-08 16:26:29 +0800

    v1.0.8 开关App Analytics SDK 替换 TalkingData Game Analytics SDK

--- bigfunglobal/src/main/java/com/topgame/sdk/TwlkHifdwa.java
+++ bigfunglobal/src/main/java/com/topgame/sdk/TwlkHifdwa.java
@@ -11,11 +11,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TwlkHifdwa {
+
     public static void hfowaEfa(Context context, String eventName, String valueName, String value) {
         Map<String, Object> map = new HashMap<String, Object>();
         map.put(valueName, value);     //级别区间，注意是字符串哟！
         TCAgent.onEvent(context,eventName,"",map);
     }
+
     public static void hfowaEfa(Context context, String eventName, Map map) {
         TCAgent.onEvent(context,eventName,"",map);
     }
